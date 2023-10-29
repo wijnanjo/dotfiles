1beea261580eb6d1c248049e7e66014fbf784d11
jo.wijnant <jo.wijnant@gmail.com>
2023-10-29 17:28:53 +0000

    make it work for oh my zsh

---
+++ terminal/oh-my-zsh.zsh
@@ -0,0 +1,7 @@
+export ZSH_THEME="powerlevel10k/powerlevel10k"
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
