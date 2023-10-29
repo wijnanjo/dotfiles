4311fed51793dd4f0815ac809937e492d69df990
$(git config --get user.name) <jo.wijnant@gmail.com>
2023-10-29 22:03:07 +0000

    dedicated devcontainer script

--- bootstrap.devcontainer.sh
+++ bootstrap.devcontainer.sh
@@ -4,9 +4,83 @@
 
 set -e
 
-SCRIPT_DIR="$HOME/.dotfiles/script"
+cd "$(dirname "$0")"
+DOTFILES_ROOT=$(pwd -P)
 
-GIT_AUTHOR_NAME=${GIT_AUTHOR_NAME:=gitname} GIT_AUTHOREMAIL=${GIT_AUTHOREMAIL:=gitemail} \
-    OVERWRITE_ALL=true ${SCRIPT_DIR}/bootstrap
+echo ''
 
-${SCRIPT_DIR}/install
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite= backup= skip=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
+    local currentSrc="$(readlink $dst)"
+
+    if [ "$currentSrc" == "$src" ]; then
+        skip=true;
+    else
+        if [[ $dst =~ gitconfig ]]; then
+            skip=true
+        else
+            overwrite=true
+        fi
+    fi
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    ln -s "$1" "$2"
+    success "linked $1 to $2"
+  fi
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
+
+  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+  do
+    dst="$HOME/.$(basename "${src%.*}")"
+    link_file "$src" "$dst"
+  done
+}
+
+install_dotfiles
+
+# find the installers and run them iteratively
+find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
