f644206fb6b5bf5e893115a58714fa941004a65d
Jo Wijnant <jo.wijnant@kuleuven.be>
2024-05-25 13:16:48 +0200

    go path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/bin:$HOME/.local/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/bin:$HOME/.local/bin:$PATH:/usr/local/go/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
